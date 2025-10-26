17cf4db1bbefb525ae3dcfbed1d00eca05c9e012
yesseniay <f99700177@gmail.com>
2025-10-26 19:27:44 -0300

    activity main
    logica de los card

--- app/src/main/java/com/example/miagenda/MainActivity.java
+++ app/src/main/java/com/example/miagenda/MainActivity.java
@@ -1,24 +1,50 @@
 package com.example.miagenda;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MaterialCardView card_contactos, card_agregar, card_notas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        card_contactos = findViewById(R.id.card_contactos);
+        card_agregar = findViewById(R.id.card_agregar);
+        card_notas = findViewById(R.id.card_notas);
+
+        //  Abre el activity contacto
+        card_contactos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ContactoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Abre el activity agregar contacto
+        card_agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AgregarContactoActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //  Abre el activity de notas
+        card_notas.setOnClickListener(v -> {
+            Toast.makeText(this, "Notas", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MainActivity.this, NotaActivity.class));
         });
     }
 }
